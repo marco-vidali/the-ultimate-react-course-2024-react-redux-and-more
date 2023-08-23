daff1cf0795d41fa1fbb7e248d305f0511f1b877
marco-vidali <marco-vidali-3@proton.me>
2023-08-23 10:53:37 +0200

    Asynchronous JavaScript: Async/Await

--- JS-review/script.js
+++ JS-review/script.js
@@ -274,7 +274,20 @@ const booksAfterUpdate = booksAfterAdd.map((book) =>
 booksAfterUpdate;
 */
 
-fetch("https://jsonplaceholder.typicode.com/todos/")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos/")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// console.log("jonas");
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const data = await res.json();
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
+
 console.log("jonas");
