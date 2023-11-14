bd498735cda0baaa3f5e9d1338ccdf611b8e5fe9
marco-vidali <marco-vidali-4@proton.me>
2023-11-14 17:10:46 +0100

    Finishing a Quiz

---
+++ 10-react-quiz/src/components/Progress.js
@@ -0,0 +1,17 @@
+function Progress({ i, numQuestions, points, maxPossiblePoints, answer }) {
+  return (
+    <header className="progress">
+      <progress max={numQuestions} value={i + Number(answer !== null)} />
+
+      <p>
+        Question <strong>{i}</strong> / {numQuestions}
+      </p>
+
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
