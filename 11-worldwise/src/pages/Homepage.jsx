c8a8562a4f75a70bdb1f11f2f31d8972532b987a
marco-vidali <marco-vidali-4@proton.me>
2023-11-19 11:19:44 +0100

    Using CSS Modules

--- 11-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
 import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
-      <h1>WorldWise</h1>
+      <AppNav />
+      <h1 className="test">WorldWise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
