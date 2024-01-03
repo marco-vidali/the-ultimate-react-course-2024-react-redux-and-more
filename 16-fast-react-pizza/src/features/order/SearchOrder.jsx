b304bc21a7381a468ecdef4b8f49035e71b9931c
marco-vidali <marco-vidali-4@proton.me>
2024-01-03 15:59:58 +0100

    Styling Form Elements

--- 16-fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 16-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -18,6 +18,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="placeholder: text-smtext-stone-400 sm:64 w-28 rounded-full bg-yellow-100 px-4 py-2 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:focus:w-72"
       />
     </form>
   );
