83efb1c11062dbc4e8f0707e985b1baaef9406c1
marco-vidali <marco-vidali-4@proton.me>
2024-01-25 16:22:57 +0100

    Converting the Modal to a Compound Component

--- 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -3,21 +3,42 @@ import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
+// function AddCabin() {
+//     const [isOpenModal, setIsOpenModal] = useState(false);
+
+//     return (
+//         <div>
+//             <Button onClick={() => setIsOpenModal((show) => !show)}>
+//                 Add new cabin
+//             </Button>
+
+//             {isOpenModal && (
+//                 <Modal onClose={() => setIsOpenModal(false)}>
+//                     <CreateCabinForm onCloseModal />
+//                 </Modal>
+//             )}
+//         </div>
+//     );
+// }
+
 function AddCabin() {
-    const [isOpenModal, setIsOpenModal] = useState(false);
-
     return (
-        <div>
-            <Button onClick={() => setIsOpenModal((show) => !show)}>
-                Add new cabin
-            </Button>
+        <Modal>
+            <Modal.Open opens="cabin-form">
+                <Button>Add new cabin</Button>
+            </Modal.Open>
+            <Modal.Window name="cabin-form">
+                <CreateCabinForm />
+                <CreateCabinForm />
+            </Modal.Window>
 
-            {isOpenModal && (
-                <Modal onClose={() => setIsOpenModal(false)}>
-                    <CreateCabinForm onCloseModal />
-                </Modal>
-            )}
-        </div>
+            {/* <Modal.Open opens="table">
+                <Button>Show table</Button>
+            </Modal.Open>
+            <Modal.Window name="table">
+                <CreateCabinForm />
+            </Modal.Window> */}
+        </Modal>
     );
 }
 
