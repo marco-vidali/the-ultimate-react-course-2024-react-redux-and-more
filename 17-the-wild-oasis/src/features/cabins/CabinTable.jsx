3be11c4f555a31f022d1f862cacde89dda9af1fc
marco-vidali <marcovidali1@skiff.com>
2024-01-30 16:16:54 +0100

    Client-Side Sorting Sorting Cabins

--- 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -30,11 +30,20 @@ function CabinTable() {
     const filterValue = searchParams.get("discount") || "all";
     let filteredCabins;
 
+    // filter
     if (filterValue === "all") filteredCabins = cabins;
     if (filterValue === "no-discount")
         filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
     if (filterValue === "with-discount")
         filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+    // sort
+    const sortBy = searchParams.get("sortBy") || "startDate-asc";
+    const [field, direction] = sortBy.split("-");
+    const modifier = direction === "asc" ? 1 : -1;
+    const sortedCabins = filteredCabins.sort(
+        (a, b) => (a[field] - b[field]) * modifier
+    );
 
     return (
         <Menus>
@@ -49,7 +58,8 @@ function CabinTable() {
                 </Table.Header>
 
                 <Table.Body
-                    data={filteredCabins}
+                    // data={filteredCabins}
+                    data={sortedCabins}
                     render={(cabin) => (
                         <CabinRow cabin={cabin} key={cabin.id} />
                     )}
