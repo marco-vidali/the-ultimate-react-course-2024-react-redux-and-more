0e6c47eee09156013aec50a2650e082ea32c9c82
marco-vidali <marcovidali1@skiff.com>
2024-02-07 16:06:08 +0100

    User Sign Up

--- 17-the-wild-oasis/src/features/authentication/SignupForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -3,21 +3,29 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-    const { register, formState, getValues, handleSubmit } = useForm();
+    const { register, formState, getValues, handleSubmit, reset } = useForm();
     const { errors } = formState;
+    const { signup, isLoading } = useSignup();
 
-    function onSubmit(data) {
-        console.log(data);
+    function onSubmit({ fullName, email, password }) {
+        signup(
+            { fullName, email, password },
+            {
+                onSettled: () => reset(),
+            }
+        );
     }
 
     return (
         <Form onSubmit={handleSubmit(onSubmit)}>
             <FormRow label="Full name" error={errors?.fullName?.message}>
                 <Input
+                    disabled={isLoading}
                     type="text"
                     id="fullName"
                     {...register("fullName", {
@@ -28,6 +36,7 @@ function SignupForm() {
 
             <FormRow error={errors?.email?.message} label="Email address">
                 <Input
+                    disabled={isLoading}
                     type="email"
                     id="email"
                     {...register("email", {
@@ -45,6 +54,7 @@ function SignupForm() {
                 error={errors?.password?.message}
             >
                 <Input
+                    disabled={isLoading}
                     type="password"
                     id="password"
                     {...register("password", {
@@ -62,6 +72,7 @@ function SignupForm() {
                 error={errors?.passwordConfirm?.message}
             >
                 <Input
+                    disabled={isLoading}
                     type="password"
                     id="passwordConfirm"
                     {...register("passwordConfirm", {
@@ -75,10 +86,10 @@ function SignupForm() {
 
             <FormRow>
                 {/* type is an HTML attribute! */}
-                <Button variation="secondary" type="reset">
+                <Button variation="secondary" type="reset" disabled={isLoading}>
                     Cancel
                 </Button>
-                <Button>Create new user</Button>
+                <Button disabled={isLoading}>Create new user</Button>
             </FormRow>
         </Form>
     );
