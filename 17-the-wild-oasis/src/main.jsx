12afdc0d17d5ea1cd807c8b3b1fe31fcfff72bd0
marco-vidali <marcovidali1@skiff.com>
2024-02-11 11:38:41 +0100

    Error Boundaries

--- 17-the-wild-oasis/src/main.jsx
+++ 17-the-wild-oasis/src/main.jsx
@@ -1,5 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
+import GlobalStyles from "./styles/GlobalStyles.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <>
+        <GlobalStyles />
+        <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+            onReset={() => window.location.replace("/")}
+        >
+            <App />
+        </ErrorBoundary>
+    </>
+);
