61e5efef1ef33cd35e48ef8e1117cbae600f6057
marco-vidali <marcovidali1@skiff.com>
2024-02-11 11:48:45 +0100

    Deploying to Netlify

--- 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-    const [email, setEmail] = useState("marco@example.com");
-    const [password, setPassword] = useState("Ciao123!");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const { login, isLoading } = useLogin();
 
     function handleSubmit(e) {
